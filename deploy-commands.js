fe710b51d771d67c0a22904446c81faef709c246
Hajïn_ <44238262+AHajin@users.noreply.github.com>
2022-06-07 15:20:09 +0200

    DB/Bot Fonctionnel
    nouveau template db de test (ou définitif a voir)
    + bot entier

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,6 +8,7 @@ const commands = [
     new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
     new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
     new SlashCommandBuilder().setName('meetup').setDescription('Replies in direct message'),
+    new SlashCommandBuilder().setName('cheerosteam').setDescription('Cheers all server members'),
     new SlashCommandBuilder().setName('cheeros').setDescription('Give a cheer to your coworker').addUserOption(option =>
         option.setName("member")
             .setDescription("target of the cheeros")
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { token, guildId, host,user,password, database} = require('./config.json')
 let mysql = require('mysql');
 let step = null
 const { userMention, memberNicknameMention, channelMention, roleMention } = require('@discordjs/builders');
-const fs = require("fs");
 let workers = {
     worker: []
 }
@@ -21,16 +20,51 @@ let response = {
     response : []
 };
 
-function writeJsonResponse(){
-    let toJson = JSON.stringify(response);
-    fileSystem.writeFile('./response.json', toJson, err=>{
-        if(err){
-            console.log("failed to register");
-        }else{
-            console.log("JSON data written");
+function stepVerif(msg){
+    const search = `SELECT step FROM workers WHERE id_user= "${msg.author.id}"`;
+    con.query(search, (err, result) => {
+        console.log(result[0].step)
+        step = result[0].step
+        console.log(step)
+        if (step === 1)
+            console.log("a")
+
+        console.log(step, msg.author.username )
+
+
+        if(step === 1){
+            console.log(step)
+            msg.author.send(`and what are you planning to do today ?`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
+        }else if(step === 2){
+            console.log(step)
+            msg.author.send(`Thank you ! Now you can prepare for the scrum meeting !`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
+        }else if(step === 3){
+            console.log(step)
+            step = 0
+            msg.author.send(`Thanks for you commitment <@${msg.author.id}>! we'll catch you later!`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
+
+        }else {
+            msg.author.send(`Thanks for you commitment <@${msg.author.id}>! we'll catch you later!`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
+
         }
+        console.log('Event');
+        const sql = `INSERT INTO response (id_user, name_user, response_obj, time, step) VALUES ("${msg.author.id}", "${msg.author.username}", "${msg.content}", "${msg.createdTimestamp}","${step}")`;
+        if(step !== 0)step++
+
+        const updateWorkers = `UPDATE workers
+                                   SET step = "${step}"
+                                   where id_user = "${msg.author.id}"`;
+
+        try{
+            if(step > 0)
+                con.query(sql)
+            con.query(updateWorkers)
+        }catch (e) {
+            console.log(e)
+        };
     })
 }
+
 function writeJsonWorkers(){
     let toJson = JSON.stringify(workers);
     fileSystem.writeFile('./scrum.json', toJson, err=>{
@@ -41,21 +75,6 @@ function writeJsonWorkers(){
         }
     })
 }
-function readWorkers(){
-
-/*    fileSystem.readFile('./scrum.json', err => {
-        if(err){
-            console.log("failed to read");
-        }else{
-            console.log("JSON date read")
-        }
-
-    })*/
-
-    let workers = fs.readFileSync('./scrum.json')
-    return JSON.parse(workers)
-}
-
 // Create a new client instance
 const client = new Client({ intents: [
         Intents.FLAGS.DIRECT_MESSAGES, //required to interact in dms
@@ -81,56 +100,10 @@ client.on("messageCreate", msg =>{
     const channel = client.channels.cache.find(channel => channel.id === `959904868355674193`)
 
 
-    let date = new Date(msg.createdTimestamp);
-    console.log(date)
-
-console.log('Event');
-    console.log(step)
-   // console.log(employee.id_user,employee.step,employee.name_user)
     if(msg.channel.type === 'DM' && msg.author.id !== '943444812747669504') {
-        //console.log(workers.worker.id_user,workers.worker.step);
-        if(step === 1){
-            console.log(step)
-            msg.author.send(`and what are you planning to do today ?`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
-            step++
-        }else if(step === 2){
-            console.log(step)
-            msg.author.send(`Thank you ! Now you can prepare for the scrum meeting !`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
-            step++
-        }else if(step === 3){
-            console.log(step)
-            msg.author.send('oops');
-            step= 0
-        }else {
-            msg.author.send(`Thanks for you commitment <@${msg.author.id}>! we'll catch you later!`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
-
-        }
-        /*console.log('Event');
-            msg.author.send(`Thanks for you commitment <@${msg.author.id}>! we'll catch you later!`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));*/
-
-
-
-            //channel.send(`**id_User : ${msg.author.id},\nUsername : ${msg.author.username},\nmessageContent : ${msg.content},\ndate : ${date}\n\n\n**`);
-            /*response.response.push({id_user : msg.author.id,name_user: msg.author.username, response_obj : msg.content, timestamp: msg.createdTimestamp});
-            writeJson();*/
-        //}
-/*
-response.response.push({id_user : msg.author.id,name_user: msg.author.username, response_obj : msg.content, time: date});
-writeJsonResponse();
-        writeJsonWorkers()*/
-        const updateWorkers = `UPDATE workers SET step = "${step}" where id_user = "${msg.author.id}"`
-        const sql = `INSERT INTO response (id_user, name_user, response_obj, time, step) VALUES ("${msg.author.id}", "${msg.author.username}", "${msg.content}", "${msg.createdTimestamp}","${step}")`;
-        try{
-            con.query(sql)
-            con.query(updateWorkers)
-        }catch (e) {
-            console.log(e)
-        };
-return;
-
+        console.log("event")
+        stepVerif(msg);
     }
-console.log(response.response);
-
 })
 
 
@@ -155,19 +128,21 @@ client.on('interactionCreate',  interaction => {
                 if(members.id !== '943444812747669504' && members.id !== '159985870458322944' && members.id !== '432610292342587392' && members.id !== '468281173072805889' && members.id !== '571027211407196161' && members.id !== '887361046032023562' ){
 
 
-                    try{
-                        const insert = `INSERT INTO workers (id_user,username,step,id_guild) VALUES ("${members.id}","${members.user.username}",1,"${interaction.guild.id}")`;
-                        con.query(insert);
 
-                    }catch (e){
-                        const search = `SELECT id_user= "${members.id}" FROM workers`;
-                        con.query(search);
-                    }
+                        const insert = `INSERT INTO workers (id_user,username,step,id_guild) VALUES ("${members.id}","${members.user.username}",1,"${interaction.guild.id}")`;
+                        con.query(insert, (err, res)=>{
+                            const search = `SELECT id_user= "${members.id}" FROM workers`;
+                            con.query(search, (err, res)=>{
+                                const updateWorkers = `UPDATE workers SET step = 1 where id_user = "${members.id}"`
+                                con.query(updateWorkers)
+                            });
+                        });
+
                     console.log(`${members.user.username} have been DMed`);
                     console.log(members.user.id,members.user.username,members.user.tag)
                     workers.worker.push({id_user : members.id,name_user: members.user.username, step: 1});
                     writeJsonWorkers();
- //members.send("What did you do yesterday ?");
+                    members.send("What did you do yesterday ?").catch(e => console.error(`Couldnt DM member ${members.user.id,members.user.username,members.user.tag}`));
 
                 }
 
@@ -177,7 +152,7 @@ client.on('interactionCreate',  interaction => {
                 }
             });
         });
-    }else if (commandName === 'cheeros') {
+    }else if (commandName === 'cheerosteam') {
         interaction.reply(`cheeros launched!`);
         //console.log(interaction.guild.members.list()) //debug
 // First use guild.members.fetch to make sure all members are cached
@@ -186,10 +161,8 @@ client.on('interactionCreate',  interaction => {
                 // Trying to send a message to the member.
                 // This method might fail because of the member's privacy settings, so we're using .catch
 
-                if(members.id === '316960380515516426'){
-                    members.send("totalement oui, je test un trucs avec des flags");
-                }else if(members.id === '209822631900217344' && members.id !== '943444812747669504' && members.id !== '159985870458322944' && members.id !== '432610292342587392' && members.id !== '468281173072805889' && members.id !== '571027211407196161' && members.id !== '887361046032023562' ) {
-                    members.send("What did you do yesterday ?");
+                if(members.id === '209822631900217344' && members.id !== '943444812747669504' && members.id !== '159985870458322944' && members.id !== '432610292342587392' && members.id !== '468281173072805889' && members.id !== '571027211407196161' && members.id !== '887361046032023562' ) {
+                    members.send(`${interaction.user.username} want to cheers you, keep up the good work !`).catch(e => console.error(`Couldnt DM member ${msg.author.tag}`));
                     console.log(`${members.user.username} have been DMed`);
                     console.log(members.user.id, members.user.username, members.user.tag)
                 }
